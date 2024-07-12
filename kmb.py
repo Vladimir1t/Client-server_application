54008ad4b316ed81eb0d1174d7d273957e4e652d
Vladimir1t <vova.vexov@mail.ru>
2024-07-12 17:29:17 +0300

    some changes

--- kmb.py
+++ kmb.py
@@ -25,119 +25,114 @@ def main():
     logging.basicConfig(filename = args.file, level = logging.INFO)
     logger.info('Started')
 
-    if args.file:
-        mode = 'f'
-    elif args.o:
-        mode = 'o'
-
     if args.udp:
-        udp_protocol(mode)
+        udp_protocol()
     elif args.tcp:
-        tcp_protocol(mode)
+        tcp_protocol()
     else:
         print('Error. Wrong flag')
 
     logger.info('Finished')
 
-def udp_protocol(mode):
+def udp_protocol():
     if args.server:
         server_socket = socket(AF_INET, SOCK_DGRAM)
         server_socket.bind(('',  args.server_port))
         print("server is ready")
-        if mode == 'f':
-            logger.info('Server is ready (socket was created) \n')
+        if args.file:
+            logger.info('Server is ready (socket was created)')
 
         while 1:
             message, client_address = server_socket.recvfrom(2048)
             modified_message = message.upper()
             print(modified_message)
             print('client address: ', client_address)
-            if mode == 'f':
-                logger.info('   Message was got. Client address: ' + str (client_address) + '\n')
+            if args.file:
+                logger.info('   Message was got. Client address: ' + str (client_address))
             server_socket.sendto(modified_message, client_address)
             server_socket.sendto(bytes(str(client_address) + ' you were connected to the server', "UTF-8"), client_address)
 
             if message == b'stop':
-                if mode == 'f':
-                    logger.info('Server process was closed \n')
+                if args.file:
+                    logger.info('Server process was closed')
                 server_socket.close()
                 break
 
     elif args.client:
         client_socket = socket(AF_INET, SOCK_DGRAM)
         print("client is ready")
-        if mode == 'f':
-            logger.info('Client is ready (socket was created) \n')
-        message = input('input sentence: {write stop to stop the server process}\n')
+        if args.file:
+            logger.info('Client is ready (socket was created)')
+        message = input('input sentence: {write \'stop\' to stop the server process} \n')
         client_socket.sendto(bytes(message, "UTF-8"), ( args.IP_server,  args.server_port))
-        if mode == 'f':
-                logger.info('   Message was sent \n')
+        if args.file:
+                logger.info('   Message was sent')
         modified_sentence, server_address = client_socket.recvfrom(2048)
         address, server_address = client_socket.recvfrom(2048)
-        if mode == 'f':
-                logger.info('   Answer was got \n')
+        if args.file:
+                logger.info('   Answer was got')
         print(modified_sentence)
         print(address)
         client_socket.close()
-        if mode == 'f':
-                logger.info('Socket was closed \n')
+        if args.file:
+                logger.info('Socket was closed')
 
     else:
         print("Error. Wrong flag")
 
-def tcp_protocol(mode):
+def tcp_protocol():
     if args.server:
         server_socket = socket(AF_INET, SOCK_STREAM)
         server_socket.bind(('',  args.server_port))
         server_socket.listen()
         print("server is ready")
-        if mode == 'f':
-            logger.info('Server is ready (socket was created) \n')
+        if args.file:
+            logger.info('Server is ready (socket was created)')
 
         while 1:
             connection_socket, client_address = server_socket.accept()
-            if mode == 'f':
-                logger.info('   Connection socket was created  (TCP-connection) \n')
+            if args.file:
+                logger.info('   Connection socket was created  (TCP-connection)')
             message = connection_socket.recv(2048)
             modified_message = message.upper()
             print(modified_message)
             print('client address: ', client_address)
-            if mode == 'f':
-                logger.info('   Answer was got. Client address: ' + str (client_address) + '\n')
+            if args.file:
+                logger.info('   Answer was got. Client address: ' + str (client_address))
 
             connection_socket.send(modified_message)
             connection_socket.send(bytes(str(client_address) + ' you were connected to the server', "UTF-8"))
-            if mode == 'f':
-                logger.info('   Connection socket was closed \n')
+            if args.file:
+                logger.info('   Connection socket was closed')
             connection_socket.close()
 
             if message == b'stop':
                 server_socket.close()
-                if mode == 'f':
-                    logger.info('Server process was closed \n')
+                if args.file:
+                    logger.info('Server process was closed')
                 break
 
     elif args.client:
         client_socket = socket(AF_INET, SOCK_STREAM)
-        if mode == 'f':
-            logger.info('Client is ready (socket was created) \n')
+        if args.file:
+            logger.info('Client is ready (socket was created)')
         client_socket.connect(( args.IP_server,  args.server_port))            # tcp-connection
         print("Client is ready")
-        if mode == 'f':
-                logger.info('   TCP-connection was created \n')
-        message = input('Input sentence: {write \'stop\' to stop the server process}\n')
+        if args.file:
+                logger.info('   TCP-connection was created')
+        message = input('Input sentence: {write \'stop\' to stop the server process} \n')
         client_socket.send(bytes(message, "UTF-8"))
-        if mode == 'f':
-                logger.info('   Message was sent \n')
+        if args.file:
+                logger.info('   Message was sent')
         modified_sentence = client_socket.recv(2048)
         address = client_socket.recv(2048)
-        if mode == 'f':
-                logger.info('   Answer was got \n')
+        if args.file:
+                logger.info('   Answer was got')
         print(modified_sentence)
         print(address)
         client_socket.close()
-        if mode == 'f':
-                logger.info('Socket was closed \n')
+        if args.file:
+                logger.info('Socket was closed')
 
     else:
         print("Error. Wrong flag")
